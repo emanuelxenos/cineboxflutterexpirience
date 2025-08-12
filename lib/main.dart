86beca1c73f6aa0057b86e893faa9d55be185c66
emanuelxenos <manuenbs@gmail.com>
2025-08-12 14:13:23 -0300

    Add Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cinebox/cinebox_main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  return runApp(CineboxMainApp());
+  return runApp(ProviderScope(child: CineboxMainApp()));
 }
