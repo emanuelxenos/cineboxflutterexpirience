f0df5cef3a3894c715216316aea9fdcd09af0e5f
emanuelxenos <manuenbs@gmail.com>
2025-08-12 15:42:10 -0300

    Config theme done

---
+++ lib/ui/core/themes/text_styles.dart
@@ -0,0 +1,44 @@
+import 'package:cinebox/ui/core/themes/colors.dart';
+import 'package:flutter/material.dart';
+
+abstract final class AppTextStyles {
+  static const titleLarge = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const subTitleSmall = TextStyle(
+    fontSize: 14,
+    color: AppColors.lightGrey,
+  );
+
+  final boldSmall = subTitleSmall.copyWith(
+    fontWeight: FontWeight.bold,
+  );
+
+  final regularSmall = subTitleSmall.copyWith(
+    fontWeight: FontWeight.w400,
+  );
+
+  static const boldMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final semiBoldMedium = boldMedium.copyWith(
+    fontWeight: FontWeight.w600,
+  );
+
+  static const lightGreyRegular = TextStyle(
+    fontSize: 14,
+    color: AppColors.lightGrey,
+  );
+
+  final lightGreySmall = lightGreyRegular.copyWith(
+    fontSize: 12,
+  );
+
+  static const errorText = TextStyle(
+    color: AppColors.redColor,
+  );
+}
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinebox/config/env.dart';
 import 'package:cinebox/ui/core/themes/resource.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
