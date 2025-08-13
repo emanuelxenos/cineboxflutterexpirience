ebb032d9952615dff7912e3e500fa43d5bd5df10
emanuelxenos <manuenbs@gmail.com>
2025-08-13 11:14:58 -0300

    Connecting google cloud for login with google

---
+++ lib/data/services/services_provider.dart
@@ -0,0 +1,12 @@
+import 'package:cinebox/data/services/local_sotrage/local_storage_service.dart';
+import 'package:cinebox/data/services/local_sotrage/local_storage_service_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'services_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+LocalStorageService localStorageService(Ref ref) {
+  return LocalStorageServiceImpl(flutterSecureStorage: FlutterSecureStorage());
+}
