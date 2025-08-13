243f555efd46142fe41ed3964f537f12473a3b18
emanuelxenos <manuenbs@gmail.com>
2025-08-12 21:42:10 -0300

    Config MessageAnLoader

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,12 +10,21 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends ConsumerState<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen>
+    with LoaderAndMessage {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Splash'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(Duration(seconds: 3));
+          hideLoader();
+          showErrorSnackBar('Error Raniere');
+        },
       ),
       body: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
     );
