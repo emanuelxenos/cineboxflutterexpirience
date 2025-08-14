feddb7d1425a084eb09cda34f467396a40e50ce4
emanuelxenos <manuenbs@gmail.com>
2025-08-14 12:42:14 -0300

    Done movieSearch screen

--- lib/ui/movies/movies_screen.dart
+++ lib/ui/movies/movies_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cinebox/ui/movies/widgets/genres_box.dart';
 import 'package:cinebox/ui/movies/widgets/movies_appbar.dart';
 import 'package:cinebox/ui/movies/widgets/movies_by_category.dart';
+import 'package:cinebox/ui/movies/widgets/movies_by_gender.dart';
+import 'package:cinebox/ui/movies/widgets/movies_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -24,7 +26,7 @@ class _MoviesScreenState extends ConsumerState<MoviesScreen> {
           ),
         ),
         SliverToBoxAdapter(
-          child: MoviesByCategory(),
+          child: MoviesSearch(),
         ),
       ],
     );
---
+++ lib/ui/movies/widgets/movies_search.dart
@@ -0,0 +1,23 @@
+import 'package:cinebox/ui/movies/widgets/movies_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MoviesSearch extends ConsumerStatefulWidget {
+  const MoviesSearch({super.key});
+
+  @override
+  ConsumerState<MoviesSearch> createState() => _MoviesSearchState();
+}
+
+class _MoviesSearchState extends ConsumerState<MoviesSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 130),
+      child: MoviesBox(
+        title: 'Filmes Encontrados Pela busca',
+        vertical: true,
+      ),
+    );
+  }
+}
